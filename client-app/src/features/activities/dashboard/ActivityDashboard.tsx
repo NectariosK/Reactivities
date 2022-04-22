3ad487fea02ddbe77751927811fe9dbfd4b0f866
NectariosK <kisiighanectarios.com>
2022-04-22 05:36:54 +0200

    End of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,27 +15,35 @@ interface Props {
     closeForm: () => void; //no paramters-returns void
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, 
-    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props){
+    selectActivity, cancelSelectActivity, editMode, openForm, closeForm, 
+    createOrEdit, deleteActivity, submitting}: Props){
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities}
-                 selectActivity = {selectActivity} 
-                 deleteActivity = {deleteActivity}
+                    selectActivity = {selectActivity} 
+                    deleteActivity = {deleteActivity}
+                    submitting = {submitting}
                  />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode && //not in editMode
                 <ActivityDetails 
-                activity={selectedActivity} 
-                cancelSelectActivity={cancelSelectActivity}
-                openForm={openForm}
+                    activity={selectedActivity} 
+                    cancelSelectActivity={cancelSelectActivity}
+                    openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm 
+                    closeForm={closeForm} 
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}
+                />}
             </Grid.Column>
         </Grid>
     )
