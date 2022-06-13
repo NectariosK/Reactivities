b22cc4042ed71727430795163448e0382831799f
NectariosK <kisiighanectarios.com>
2022-06-13 19:50:55 +0200

    Refactoring the app to use MobX

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -26,7 +26,7 @@ namespace API.Extensions
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                  {
-                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://192.168.1.19:3000/");
                  });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,25 +6,19 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import {v4 as uuid} from 'uuid'; //universelly unique identifier-using the GUID for the activity id
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
+  const {activityStore} = useStore();
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSeletectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false); //initial state as false
-  const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = [];
-      response.forEach(activity =>{
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity)
-      })
-      setActivities(activities);
-      setLoading(false);
-    })
-  }, [])
+    activityStore.loadActivities();
+  }, [activityStore])
 
 
   function handleSelectActivity(id: string){
@@ -73,14 +67,14 @@ function App() {
     
   }
 
-  if (loading) return <LoadingComponent content='Loading app' />
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
   return (
     <>
       <NavBar openForm = {handleFormOpen}/>
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard 
-        activities={activities}
+        <ActivityDashboard
+        activities={activityStore.activities}
         selectedActivity = {selectedActivity}
         selectActivity = {handleSelectActivity}
         cancelSelectActivity = {handleCancelSelectActivity}
@@ -96,4 +90,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
